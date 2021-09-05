036ffbd49c798e294145fbc401eced931da59cab
sharif1014 <sharifuliuk@gmail.com>
2021-09-05 16:26:12 +0600

    header added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import Topbar from "./components/Topbar"
 import './App.css';
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
       <Topbar/>
+      <br/>
+      <br/>
+      <Header/>
     </div>
   );
 }
